e4a0fdbe4a46b2e70830786175a27af07c68581d
Naichen Zhao <nzhao11235@berkeley.edu>
2023-12-22 12:29:36 -0800

    pain

--- pendulum_lowlevel_arduino/src/main.cpp
+++ pendulum_lowlevel_arduino/src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
-#include <SPI.h>
 
 #define motorInterfaceType 1
 AccelStepper step_motor = AccelStepper(motorInterfaceType, 5, 4);
@@ -13,53 +12,61 @@ int sensor_val = 0;
 #define RX1 2
 #define TX1 3
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, RX1, TX1);
 
-  // step_motor.setCurrentPosition(0);
-  // step_motor.setMaxSpeed(400000);
-  // step_motor.setAcceleration(150000000);
-
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  step_motor.setCurrentPosition(0);
+  step_motor.setMaxSpeed(400000);
+  step_motor.setAcceleration(150000000);
 }
 
-void loop() {
-  float curr_angle = -((float)sensor_val)/1000;
+void loop()
+{
+  float curr_angle = -((float)sensor_val) / 1000;
 
-  int getspeed = 100 * (int)curr_angle;
+  int getspeed = 10000 * (int)curr_angle;
 
-  if (curr_angle > 45 || curr_angle < -45) {
+  if (curr_angle > 45 || curr_angle < -45)
+  {
     set_motor(0);
-  } else {
+  }
+  else
+  {
     set_motor(getspeed);
+  }
+  for (int i = 0; i < 8000; i++) {
+    set_motor(20000);
+  }
+
+    for (int i = 0; i < 8000; i++) {
+    set_motor(-20000);
   }
 
   Serial.print(curr_angle);
   Serial.print("  ");
-  Serial.println(getspeed);
+  Serial.print(getspeed);
+  Serial.print("  ");
+  Serial.println(step_motor.speed());
 
-  if (Serial1.available()) {
+  if (Serial1.available())
+  {
     String ser_read = Serial1.readStringUntil('\n');
     sensor_val = ser_read.toInt();
   }
 }
 
+void set_motor(int speed)
+{
 
-void set_motor(int speed) {
-
-  speed = (speed > 255)? 255:speed;
-  speed = (speed < -255) ? -255 : speed;
-
-  if (speed > 0) {
-    analogWrite(5, speed);
-    analogWrite(4, 0);
-  } else {
-    analogWrite(4, -speed);
-    analogWrite(5, 0);
+  if (speed == 0)
+  {
+    step_motor.stop();
   }
-
+  else
+  {
+    step_motor.setSpeed(speed);
+    step_motor.runSpeed();
+  }
 }
-
-
